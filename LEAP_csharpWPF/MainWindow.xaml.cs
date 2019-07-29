7f33c0bd50ca463116fcbdeb27c0e2f812a6c03e
wsfigox <wsfigox@outlook.com>
2019-07-29 22:50:18 +0800

    add new working project

---
+++ LEAP_csharpWPF/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using Leap;
+
+namespace LEAP_csharpWPF
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Sample sample; //sample类
+        //Thread sample_thread = null;//投币器进程
+        private Controller controller = new Controller();
+        private SampleListener listener;
+        public MainWindow() {                   
+
+            InitializeComponent();
+            //Sample sample = new Sample();
+            this.listener = new SampleListener();
+            this.controller = new Controller();
+
+            // Have the sample listener receive events from the controller
+            this.controller.AddListener(listener);
+
+            // Keep this process running until Enter is pressed
+            Console.WriteLine("Press Enter to quit...");
+            Console.ReadLine();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //sample = new Sample();
+            /*try {
+                sample_thread = new Thread(new ThreadStart(sample.initSample));
+                sample_thread.Start();
+            }  //初始化选择COM口
+            catch {
+                MessageBox.Show("leap启动失败"); Application.Current.Shutdown();
+            }*/
+            SampleListener listener = new SampleListener();
+            Controller controller = new Controller();
+
+            // Have the sample listener receive events from the controller
+            controller.AddListener(listener);
+
+            // Keep this process running until Enter is pressed
+            Console.WriteLine("Press Enter to quit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LEAP_csharpWPF/Sample.cs
@@ -0,0 +1,134 @@
+﻿/******************************************************************************\
+* Copyright (C) 2012-2014 Leap Motion, Inc. All rights reserved.               *
+* Leap Motion proprietary and confidential. Not for distribution.              *
+* Use subject to the terms of the Leap Motion SDK Agreement available at       *
+* https://developer.leapmotion.com/sdk_agreement, or another agreement         *
+* between Leap Motion and you, your company or other organization.             *
+\******************************************************************************/
+using System;
+using System.Threading;
+using Leap;
+
+class SampleListener : Listener
+{
+    private Object thisLock = new Object();
+
+    private void SafeWriteLine(String line)
+    {
+        //lock (thisLock)
+        {
+            Console.WriteLine(line);
+        }
+    }
+
+    public override void OnInit(Controller controller)
+    {
+        SafeWriteLine("Initialized");
+    }
+
+    public override void OnConnect(Controller controller)
+    {
+        SafeWriteLine("Connected");
+        controller.EnableGesture(Gesture.GestureType.TYPE_CIRCLE);
+        controller.EnableGesture(Gesture.GestureType.TYPE_KEY_TAP);
+        controller.EnableGesture(Gesture.GestureType.TYPE_SCREEN_TAP);
+        controller.EnableGesture(Gesture.GestureType.TYPE_SWIPE);
+    }
+
+    public override void OnDisconnect(Controller controller)
+    {
+        //Note: not dispatched when running in a debugger.
+        SafeWriteLine("Disconnected");
+    }
+
+    public override void OnExit(Controller controller)
+    {
+        SafeWriteLine("Exited");
+    }
+
+    public override void OnFrame(Controller controller)
+    {
+        // Get the most recent frame and report some basic information
+        Frame frame = controller.Frame();
+
+        SafeWriteLine("Frame id: " + frame.Id
+                    + ", timestamp: " + frame.Timestamp
+                    + ", hands: " + frame.Hands.Count
+                    + ", fingers: " + frame.Fingers.Count
+                    + ", tools: " + frame.Tools.Count
+                    + ", gestures: " + frame.Gestures().Count);
+        #region hand
+        /*
+        foreach (Hand hand in frame.Hands)
+        {
+            SafeWriteLine("  Hand id: " + hand.Id
+                        + ", palm position: " + hand.PalmPosition);
+            // Get the hand's normal vector and direction
+            Vector normal = hand.PalmNormal;
+            Vector direction = hand.Direction;
+
+            // Calculate the hand's pitch, roll, and yaw angles
+            SafeWriteLine("  Hand pitch: " + direction.Pitch * 180.0f / (float)Math.PI + " degrees, "
+                        + "roll: " + normal.Roll * 180.0f / (float)Math.PI + " degrees, "
+                        + "yaw: " + direction.Yaw * 180.0f / (float)Math.PI + " degrees");
+
+            // Get the Arm bone
+            Arm arm = hand.Arm;
+            SafeWriteLine("  Arm direction: " + arm.Direction
+                        + ", wrist position: " + arm.WristPosition
+                        + ", elbow position: " + arm.ElbowPosition);
+
+            // Get fingers
+            foreach (Finger finger in hand.Fingers)
+            {
+                SafeWriteLine("    Finger id: " + finger.Id
+                            + ", " + finger.Type.ToString()
+                            + ", length: " + finger.Length
+                            + "mm, width: " + finger.Width + "mm");
+
+                // Get finger bones
+                Bone bone;
+                foreach (Bone.BoneType boneType in (Bone.BoneType[])Enum.GetValues(typeof(Bone.BoneType)))
+                {
+                    bone = finger.Bone(boneType);
+                    SafeWriteLine("      Bone: " + boneType
+                                + ", start: " + bone.PrevJoint
+                                + ", end: " + bone.NextJoint
+                                + ", direction: " + bone.Direction);
+                }
+            }
+
+        }
+        
+        if (!frame.Hands.IsEmpty || !frame.Gestures().IsEmpty)
+        {
+            SafeWriteLine("");
+        }
+        */
+        #endregion
+    }
+
+}
+
+class Sample
+{
+    //public void initSample()
+    public Sample()
+    {
+        // Create a sample listener and controller
+        SampleListener listener = new SampleListener();
+        Controller controller = new Controller();
+
+        // Have the sample listener receive events from the controller
+        controller.AddListener(listener);
+
+        // Keep this process running until Enter is pressed
+        Console.WriteLine("Press Enter to quit...");
+        Console.ReadLine();
+
+        // Remove the sample listener when done
+        //controller.RemoveListener(listener);
+        //controller.Dispose();
+    }
+}
+
